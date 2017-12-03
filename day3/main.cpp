1e678a3b02eeedae7ecec86c11e8aee6df11d7ad
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-12-03 22:15:00 +0100

    commentified day3

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,62 +1,12 @@
- /* 
-    x x x x x
-    x 5 4 3 x
-    x 6 1 2 x
-    x 7 8 9 x
-    x x x x x
-
-
-
-    1   | 2 3 4 5 6 7 8 9   | 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25   | ...       | ...
-    x1  | x8                | (3*4 + 4)=x16                                     | x24       | 
-
-    9 2 3     3 4 5      5 6 7     7 8 9
-    24 25 10 11 12   12 13 14 15 16     16 17 18 19 20   20 21 22 23 24
-
-    0: 1
-    1: ((1 + (1-1)*2)*4 + 4) = 8
-    2: ((2 + (2-1)*2)*4 + 4) = 16
-    3: ((3 + (3-1)*2)*4 + 4) = 24
-    4: (7*4 + 4) = 32
-
-    1 -- 1 -- (1-1)*2 + 1
-    2 -- 3 -- (2-1)*2 + 1
-    3 -- 5 -- (3-1)*2 + 1
-    4 -- 7 -- (4-1)*2 + 1
-    5 -- 9
-
-
-
-    x = 11
-    if (x == 1) {
-        // x er i ring 1
-    }
-
-    for (int i = 1; i < x; i+=2) {
-        if (x < (i*4 + 4)) {
-            // x er i ring i
-        }
-    }
-
-    // finn ut hvilken ring det er i:
-    //      (for-løkka over)
-    // finn ut hvilken side det er i
-    // finn distansen til midten av siden
-    //
-    // manhattan = ring + distansen til midten
-
- */
-
 #include <iostream>
 #include <vector>
 #include <array>
 
-int whichRing(int num) {
-}
-
+// regn ut steps for å komme seg fra num'te element i snegle til midten 
+// ved manhattan distance
 int steps(int num) {
 
-    if (num == 1) {
+    if (num == 1) {     // ..er allerede i midten
         return 0;
     }
     
@@ -75,8 +25,7 @@ int steps(int num) {
     }
 
 
-    // finn ut hvilken side det er i
-        // finn de fire sidene?
+    // finn ut hvilken side det er i, og regn diff til midten
     int middle = lastInRing - ring;
     int smallestDiff = std::abs(num - middle);
     for (int i = 0; i < 3; ++i) {
@@ -86,13 +35,14 @@ int steps(int num) {
             smallestDiff = diff;
         }
     }
-
-    // finn distansen til midten av siden
     
     // manhattan = ring + distansen til midten
     return smallestDiff + ring;
 }
 
+// (ikke spesielt stolt av denne)
+// finn svaret ved å regne på samme måte som en ville gjort på papir..
+// tegn inn i 2-dimensjonelt array rundt og rundt til funnet svar
 int part2(int input) {
 
     std::array<std::array<int, 21>, 21> board = {};
