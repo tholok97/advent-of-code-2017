e3e7936870636f1b0b38363825d67da9d8080221
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-12-08 17:51:17 +0100

    bugfix in part 1. implemented part 2

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+#include <utility>
 #include <fstream>
 #include <sstream>
 #include <map>
 #include <string>
 
-void p(std::string text) {
-    std::cout << text << std::endl;
-}
-
-
+// return result on evaluating 'lhs' 'op' 'rhs', where op is a valid operator
+// ex:                         3     "==" 4      -> would return false
 bool checkCondition(auto lhs, std::string op, auto rhs) {
 
     // can't switch on strings :((((
@@ -25,18 +23,23 @@ bool checkCondition(auto lhs, std::string op, auto rhs) {
     
 }
 
-auto part1(std::string inputFile) {
+// Perform both part 1 and part 2 in one function
+// Part 1 will be returned in .first, part 2 in .second
+std::pair<int, int> bothParts(std::string inputFile) {
 
     std::ifstream ifs(inputFile);
-    std::map<std::string, int> registers;
+    std::map<std::string, int> registers;   // map of register values
+
+    // store largest register value ever (for part 2)
+    int largestEver = 0;
 
     // read instructions from file
     std::string line;
     while (getline(ifs, line)) {
 
-        std::istringstream iss(line);
+        std::istringstream iss(line); // for simplicities' sake
 
-        // Read instruction line. Structure is: (with example)
+        // Read instruction line. Structure is (with example):
         // reg operation operationAmount 'if' conditionReg conditionOp conditionAmount;
         // b   inc       5               if   a            >           1
         std::string reg, operation, conditionReg, conditionOp, temp;
@@ -45,39 +48,36 @@ auto part1(std::string inputFile) {
         iss >> reg >> operation >> opertionAmount >> temp >> 
                 conditionReg >> conditionOp >> conditionAmount;
 
-//        std::cout << reg << " "<< operation << " "<< opertionAmount << " if " << 
-//                conditionReg << " "<< conditionOp << " "<< 
-//                conditionAmount << std::endl;
-
         // if we are going to decrement, just *= -1 the amount to simplify rest 
         //  of code
         if (operation == "dec") {
-            opertionAmount *= 1;
+            opertionAmount *= -1;
         }
 
         // check conditional (relies on map initializing new entries with 0)
         if (checkCondition(registers[conditionReg], conditionOp, conditionAmount)) {
             registers[reg] += opertionAmount;
+            largestEver = std::max(registers[reg], largestEver);    // update largestEver
         }
     }
     
     // find largest value of a register: (loop through and find largest)
     auto largest = registers.begin()->second;
-    std::cout << "initial largest " << largest << std::endl;
     for (const auto& pair : registers) {
-        std::cout << pair.first << '\t' << pair.second << " ? " << largest << std::endl;
         largest = std::max(pair.second, largest);
     }
 
-    // return largest
-    return largest;
+    // return largests
+    return std::make_pair(largest,largestEver);
 }
 
 int main() {
 
+    // .first gies part 1, .second gives part 2
+    auto results = bothParts("input/input_day8.txt");
 
-    p("Test: " + std::to_string(part1("input/input_day8_test.txt")));
-    p("Part 1:" + std::to_string(part1("input/input_day8.txt")));
+    std::cout << "Part 1:" << results.first << std::endl;
+    std::cout << "Part 2:" << results.second << std::endl;
 
     return 0;
 } 
