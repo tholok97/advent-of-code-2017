69df0115f887eda601ca93b1acad42a5ea0eed3d
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-12-11 15:58:25 +0100

    original day 11 solution -> %_old, new solution as day11.cpp

--- day11.cpp
+++ day11.cpp
@@ -1,3 +1,10 @@
+/*
+ * DISCLAIMER: This my solution to day 11 AFTER looking at the reddit 
+   discussion. I based my solution on this comment: https://www.reddit.com/r/
+   adventofcode/comments/7izym2/2017_day_11_solutions/dr2nuso/
+ * My original solution is found in 'day11_old.cpp'
+ */
+
 #include <iostream>
 #include <array>
 #include <vector>
@@ -46,162 +53,66 @@ Path readInput(std::string inputFile) {
     return path;
 }
 
-// Tranformation in path. Occurrances of first + second should be replaced
-//  with result (to shorten)
-struct Transformation {
-    std::string first;
-    std::string second;
-    std::string result;
-};
+// do steps while keeping track of position in coordinate system. Keep track 
+// of minimumDistance to end of path, and maximum distance away from origin 
+// along the way
+std::pair<int,int> bothParts(Path path) {
 
-// tranform path by t. Return reduced vector as .first, and vector of new 
-//  values as .second
-std::pair<Path, Path> transformReplace(Path path, Transformation t) {
+    // minLength and furthestLength are values to be returned
+    // length is the current length during iterations
+    int minLength, furthestLength, length;
+    
+    // coordinates
+    int x = 0;
+    int y = 0;
+    int z = 0;
 
-    Path toadd;     // values to add as a result of transform
-
-    // tranform until can't find another pair to tranform
-    int firstIndex, secondIndex;
-    do {
-        firstIndex = -1;
-        secondIndex = -1;
-
-        // seek after pair to transform
-        for (int i = 0; i < path.size(); ++i) {
-            if (firstIndex != -1) {
-                if (path[i] == t.second) {
-                    secondIndex = i;
-                    break;
-                }
-            } else {
-                if (path[i] == t.first) {
-                    firstIndex = i; 
-                }
-            }
-        }
-
-        // if found pair -> reduce from path and add result to toadd
-        if (firstIndex != -1 && secondIndex != -1) {
-            path.erase(path.begin()+firstIndex);
-            path.erase(path.begin()+secondIndex-1);
-            if (t.result != "") {
-                toadd.push_back(t.result);
-            }
-        }
-    } while (firstIndex != -1 && secondIndex != -1);
-
-    return std::make_pair(path, toadd);
-}
-
-// Shorten path by applying tranformations
-Path shorten(Path path) {
-
-    // transformations that apply to hex paths (found these with pen and paper)
-    static std::array<Transformation, 7> transforms = {{
-        {"n", "s", ""},
-        {"ne", "nw", "n"},
-        {"se", "sw", "s"},
-        {"n", "se", "ne"},
-        {"n", "sw", "nw"},
-        {"s", "nw", "sw"},
-        {"s", "ne", "se"}
-    }};
-
-
-    // apply transformations (both {first, second} and {second, first})
-    Path toadd;
-    for (auto t : transforms) {
-
-        // do transform. store reduced and toadd
-        auto result = transformReplace(path, t);
-        path = result.first;
-        toadd.insert(toadd.end(), result.second.begin(), result.second.end());
-
-        // do other way around
-        std::swap(t.first, t.second);
-
-        // do transform. store reduced and toadd
-        result = transformReplace(path, t);
-        path = result.first;
-        toadd.insert(toadd.end(), result.second.begin(), result.second.end());
-        path.insert(path.end(), toadd.begin(), toadd.end());
-
-        // add toadd back to the path (not storing toadd seperately caused 
-        //  problems.. I don't understand why tbh)
-        toadd.erase(toadd.begin(), toadd.end());
-    }
-
-    return path;
-}
-
-// find length of path
-int lengthOfPath(Path path) {
-
-    // count positions in three 'dimensions'
-    int n = 0;
-    int ne = 0;
-    int nw = 0; 
-
-    // for each step -> go in that direction
+    // for each step in the path, do the step and update variables
     for (auto step : path) {
         
-        // (can't switch on strings :( )
-        if        (step == "n") {   n++;
-        } else if (step == "s") {   n--;
-        } else if (step == "ne") {  ne++;
-        } else if (step == "sw") {  ne--;
-        } else if (step == "nw") {  nw++;
-        } else if (step == "se") {  nw--;
+        // ( can't switch on strings :(  )
+        if (step == "n") {
+            y++;
+            z--;
+        } else if (step == "ne") {
+            x++;
+            z--;
+        } else if (step == "nw") {
+            x--;
+            y++;
+        } else if (step == "s") {
+            y--;
+            z++;
+        } else if (step == "se") {
+            y--;
+            x++;
+        } else if (step == "sw") {
+            x--;
+            z++;
         } else {
-            std::cout << "malformed step: " << step << std::endl;
+            std::cout << "ERROR: unknown step type: " << step << std::endl;
         }
+
+        length = (abs(x) + abs(y) + abs(z))/2;
+
+        // store maximum length seen so far
+        furthestLength = std::max(furthestLength, length);
     }
 
-    // length is sum of abs of position
-    return abs(n) + abs(ne) + abs(nw);
-}
+    // minimum length is the last length
+    minLength = length;
 
-// part 1: find distance from origin
-int part1(Path path) {
-    path = shorten(path);
-    return lengthOfPath(path);
-}
-
-// part 2: find max distance from origin during path 
-// (WARNING: slow. takes a while)
-int part2(Path path) {
-
-    int max = 0;            // current max distance
-    int counter = 0;        // used for debug output
-
-    // find length of shortest path to the end of path, [update max, ] 
-    //  reduce path by one, repeat
-    while (path.size() > 0) {
-        int dist = part1(path);         // shortest distance to end of path
-        max = std::max(dist, max);      // [update max]
-        path.pop_back();                // reduce path by one
-
-        // print debug info every 100'th iteration
-        counter++;
-        if (counter >= 100) {
-            counter = 0;
-            std::cout << "path.size(): " << path.size() << ", max: " << max << std::endl;
-        }
-    }
-
-    return max;
+    return std::make_pair(minLength, furthestLength);
 }
 
 int main() {
 
     auto path = readInput("input/input_day11.txt");
 
-    std::cout << "Part 1: " << part1(path) << std::endl;
+    auto both = bothParts(path);
 
-    std::cout << "WARNING! Part 2 takes several minutes. "
-            "Debug info will be printed along the way." << std::endl;
-
-    std::cout << "Part 2: " << part2(path) << std::endl;
+    std::cout << "Part 1: " << both.first << std::endl;
+    std::cout << "Part 2: " << both.second << std::endl;
 
 
     return 0;
