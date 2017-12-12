aad8c2f7c617f75f91e18f3a38f6c2a6cf917721
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-12-12 19:18:41 +0100

    Fixed up day12. IT'S WORKING. PUSH IT

--- day12.cpp
+++ day12.cpp
@@ -1,71 +1,90 @@
 #include <iostream>
-#include <set>
 #include <sstream>
 #include <string>
-#include <map>
 #include <vector>
 #include <fstream>
 
-using Node = std::pair<std::vector<int>, int>;
+struct Node {
+    std::vector<int> neighborList;      // list of indexes of neighboring nodes
+    int group;                          // group it belongs to (0 if none)
+};
 
-int count(std::map<int, Node> &nodes, int curr, int id) {
+// give each node connected to 'curr' the id 'id'. Also return number of nodes
+// in that group
+int groupify(std::vector<Node> &nodes, int curr, int id) {
 
-    if (nodes[curr].second) {
+    // if already has group -> we don't care about this node -> early return
+    if (nodes[curr].group != 0) {
         return 0; 
     }
 
-    nodes[curr].second = id;
+    nodes[curr].group = id;     // set group id
 
+    // sum up number of neighbors + 1 (itself)
     int sum = 1;
+    for (auto i : nodes[curr].neighborList) {
 
-    for (auto i : nodes[curr].first) {
-        sum += count(nodes, i, id);
+        // use same function recursively with neighbor index
+        sum += groupify(nodes, i, id);
     }
 
     return sum;
 }
 
-int main() {
+// read input and return vector of nodes.
+// NOTE: function relies on nodes having sequential ids in the file, as well 
+//  as the ids being the row number of the node in the file
+// (Could have avoided this with use of map, but chose vector because input was
+//  ordered nicely)
+std::vector<Node> readInput(std::string inputFile) {
 
-    std::map<int, Node> nodes;
+    std::vector<Node> nodes;
+    std::ifstream ifs(inputFile);
 
-    std::ifstream ifs("input/input_day12.txt");
+    // temp variables used for reading:
+    int node, neighbor;
+    char dummyChar;
+    std::string dummyStr;
 
+    // read all nodes in the file into 'nodes'
     std::string line;
     while (std::getline(ifs, line)) {
 
         std::istringstream iss(line);
 
-        int node;
-        iss >> node; // read node
+        iss >> node;                    // read node
 
+        nodes.push_back(Node{{}, 0});   // add node to vector (no group)
 
-        nodes[node].second = 0;
+        iss >> dummyStr;                // skip arrows
 
-        std::string dummyStr;
-        iss >> dummyStr; // skip arrows
-
-        int neighbor;
-        char dummyChar;
+        // add neighbors to neighborslist
         while (iss >> neighbor) {
-
-
-            nodes[node].first.push_back(neighbor);
-            
-            iss >> dummyChar;
+            nodes.back().neighborList.push_back(neighbor);
+            iss >> dummyChar;                               // read comma
         }
 
     }
 
-    std::set<int> seen;
+    return nodes;
+}
 
-    for (int i = 0; i < 2000; ++i) {
-        count(nodes, i, i+1);
+int main() {
 
-        seen.insert(nodes[i].second);
+    auto nodes = readInput("input/input_day12.txt");
+
+    // groupify all nodes, and count up new groups found
+    int numGroups = 0, groupID;
+    for (int i = 0; i < nodes.size(); ++i) {    // try and groupify all elements
+        groupID = i+1;                          // generate unique id for group
+        groupify(nodes, i, groupID);        
+        if (nodes[i].group == groupID) { // if a new group was found this node
+            ++numGroups;                 //  will have the id we groupified with
+        }
     }
 
-    std::cout << seen.size() << std::endl;
+    std::cout << "Part 1: " << nodes[0].group << std::endl;
+    std::cout << "Part 2: " << numGroups << std::endl;
 
     return 0;
 } 
