e4ceefc20e1864ee317d56215aa1fe6bd5e9860a
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-12-21 23:56:57 +0100

    added day14 part 1 & 2

--- day14.cpp
+++ day14.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <bitset>
+#include <stack>
+#include <array>
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <sstream>
+
+//-------------------------------- FROM DAY 10 ---------------------------------
+
 
 // return new vector with positions from startpos to startpos+length reversed
 // (circular / wraps around)
@@ -170,29 +174,31 @@ int hexToInt(char c) {
     return num;
 }
 
-std::bitset<4> toBin(int num) {
+std::array<bool, 4> toBin(int num) {
 
-    std::bitset<4> bits;
+    std::array<bool, 4> bits;
 
     for (int i = 3; i >= 0; --i) {
-        bits.set(i, num % 2);
+        bits[i] = num % 2;
         num /= 2;
     }
     
     return bits;
 }
 
-std::bitset<128> hashToBin(std::string hash) {
+std::array<bool, 128> hashToBin(std::string hash) {
 
-    std::bitset<128> bits;
+    std::array<bool, 128> bits;
 
     int pos = 0;
     for (char c : hash) {
         auto cbits = toBin(hexToInt(c));
         for (int i = 0; i < 4; ++i) {
-            bits.set(pos++, cbits[i]);
+            bits[pos++] = cbits[i];
         }
     }
+
+    return bits;
 }
 
 // do knot hash on input string. Returns hash string
@@ -216,15 +222,112 @@ std::string knotHash(std::string input) {
     return hash;
 }
 
-int part1(std::string input) {
+//------------------------------- NEW IN DAY 14 --------------------------------
+
+// Grid is 128x128 bool values
+using Grid = std::array<std::array<bool, 128>, 128>;
+
+// find number of regions in grid. A region is a collection of true values in 
+// the grid that neighbor each other. See aoc description for more detail 
+// Ex:
+// 1 1 0 0
+// 1 0 0 1
+// 0 0 0 1
+//
+// ^ this grid has 2 regions
+int numRegions(Grid grid) {
+
+    int num = 0;                    // stores the number of regions
+
+    struct Pos { int i; int j; };   // position on the grid
     
-    // WIP
-    return 0;
+    std::stack<Pos> posStack;       // stack used for search
+    Grid isSeen{};                  // used to keep track of seen positions
+
+    // lambda to generate vector of neighboring positions
+    auto genNeighbors = [](Pos p) {
+        std::vector<Pos> neighbors;
+        if (p.i > 0)        neighbors.push_back(Pos{p.i-1,  p.j});
+        if (p.j > 0)        neighbors.push_back(Pos{p.i,    p.j-1});
+        if (p.i < 127)      neighbors.push_back(Pos{p.i+1,  p.j});
+        if (p.j < 127)      neighbors.push_back(Pos{p.i,    p.j+1});
+        return neighbors;
+    };
+
+    // for each cell in the grid:
+    for (int i = 0; i < 128; ++i) {
+        for (int j = 0; j < 128; ++j) {
+
+            // if we haven't visited it before, and it's set -> it's a new 
+            // region. search through the region to set all squares in the 
+            // region to seen
+            if (!isSeen[i][j] && grid[i][j]) {
+
+                posStack.push(Pos{i, j});   // stack of positions to visit
+                Pos curr;                   // stores current pos in search
+
+                // while there are postions to visit in the region -> visit one 
+                // at a time and add all it's neighbors to the stack
+                while (!posStack.empty()) {
+
+                    // pop pos of the stack and set it to seen
+                    curr = posStack.top();
+                    posStack.pop();
+                    isSeen[curr.i][curr.j] = true;
+
+                    // for each neighbor (that is also set, and isn't seen 
+                    // yet) -> add to stack
+                    for (Pos neighbor : genNeighbors(curr)) {
+                        if (!isSeen[neighbor.i][neighbor.j] && grid[neighbor.i][neighbor.j]) {
+                            posStack.push(neighbor);
+                        }
+                    }
+                }
+
+                // we've seen a new region. increment region count
+                ++num;
+            }
+        }
+    }
+
+    return num;
+}
+
+// count number of squares set to true in the grid (for part 1)
+int numSet(const Grid& grid) {
+    int count = 0;
+    for (const auto& row : grid)
+        for (const auto& square : row)
+            if (square)
+                ++count;
+
+    return count;
+}
+
+// generate grid from string input
+Grid generate(std::string input) {
+    
+    Grid grid{};
+
+    // for each i=0..127 generate a string [input]-i and binHash it. grid[i] is 
+    // the binHash of that string
+    for (int i = 0; i < 128; ++i) {
+        auto rowInput = input;
+        rowInput.append("-");
+        rowInput.append(std::to_string(i));
+
+        grid[i] = hashToBin(knotHash(rowInput));
+    }
+
+    return grid;
 }
 
 int main() {
 
-    std::cout << "koko\n";
+    auto grid = generate("nbysizxe");
+
+    std::cout << numSet(grid) << std::endl;
+    std::cout << numRegions(grid) << std::endl;
 
     return 0;
 } 
