dad7ea9b60722d1ff429982b486f773f72a62bef
tholok97 <thomahl@stud.ntnu.no>
2018-06-19 22:37:31 +0200

    basic grunt work of part 1 day19 done

--- day19.cpp
+++ day19.cpp
@@ -1,8 +1,63 @@
+/* THE WAY I UNDERSTAND THE TASK:
+
+GIVEN:
+
+     |
+     |  +--+
+     A  |  C
+ F---|----E|--+
+     |  |  |  D
+     +B-+  +--+
+
+THE "PACKET" STARTS ABOVE A AND TRAVELS FOLLOWING THE LINE. WHAT LETTERS DOES IT SEE ALONG IT'S PATH (TO F)? 
+
+SOLUTION IN THIS CASE: ABCDEF
+
+ */
+
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using Grid = std::vector<std::vector<char>>;
+
+/**
+ * Parse file at 'inputFile' into Grid and return
+ */
+Grid parseInput(std::string inputFile) {
+
+    Grid grid;
+
+    // read input from file into 'input' 
+    std::ifstream ifs(inputFile);
+
+    // for each line in file
+    std::string line;
+    while (ifs >> line) {
+
+        // make new subvector and fill with string
+        grid.push_back(std::vector<char>(line.begin(), line.end()));
+    }
+
+    return grid;
+}
+
+/**
+ * Solves part 1 of puzzle using given grid. Returns visited characters
+ */
+std::string part1(const Grid& grid) {
+
+    // SOLVE PART 1 HERE
+
+    return "";
+}
 
 int main() {
 
-    std::cout << "Hello from day19!" << std::endl;
+    Grid grid = parseInput("input/input_day19_test.txt");
+
+    std::cout << "Part 1: " << part1(grid) << std::endl;
 
     return 0;
 }
