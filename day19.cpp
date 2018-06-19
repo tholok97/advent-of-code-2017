8d7ddebaa889ab1adaf9a79bd4477df8bbfe0954
tholok97 <thomahl@stud.ntnu.no>
2018-06-19 23:07:42 +0200

    WIP day19 part1

--- day19.cpp
+++ day19.cpp
@@ -34,7 +34,7 @@ Grid parseInput(std::string inputFile) {
 
     // for each line in file
     std::string line;
-    while (ifs >> line) {
+    while (getline(ifs, line)) {
 
         // make new subvector and fill with string
         grid.push_back(std::vector<char>(line.begin(), line.end()));
@@ -44,11 +44,46 @@ Grid parseInput(std::string inputFile) {
 }
 
 /**
+ * Stores position in (x,y)
+ */
+struct Position {
+    int x;
+    int y;
+};
+
+/**
  * Solves part 1 of puzzle using given grid. Returns visited characters
  */
 std::string part1(const Grid& grid) {
 
-    // SOLVE PART 1 HERE
+    // initial position of packet set to (0,0).
+    Position pos{0,0};
+
+    // seek until found starting position of packet (line at top edge of grid)
+    while (grid[pos.y][pos.x] != '|') {
+
+        // seek one x to the right
+        ++pos.x;
+    }
+
+    // (pos is now correctly positioned at start of packet path)
+    
+    enum Direction { UP, RIGHT, DOWN, LEFT } dir = Direction::DOWN;
+
+    //while (true) {
+
+        char currentChar = grid[pos.y][pos.x];
+
+        // switch on dir
+        // DOWN:
+        //  IF NEXT IS |, CONTINUE
+        //  IF NEXT IS CHARACTER, CONTINUE
+        //  IF NEXT IS +, EXECUTE INTERSECTION CODE
+        //  IF NEXT IS -, EXECUTE CROSSOVER CODE
+
+    //}
+
+
 
     return "";
 }
